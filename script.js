8431eb9e10f1f4767aec32108365403a537de5da
Marcin <xcinek1337@wp.pl>
2023-06-12 12:44:47 +0200

    error: unknown switch `m

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello from Nerdbord!')
+console.log('Hello from Nerdbord!');
+
+const boxBorder = document.querySelector('.box');
+
+setInterval(() => {
+	const colors = ['lightgreen', 'lightcoral', 'lightsalmon', 'lightblue'];
+	const randomColor = colors[Math.floor(Math.random() * colors.length)];
+	console.log(randomColor);
+
+    boxBorder.style.border= `40px solid ${randomColor}`
+}, 4000);
